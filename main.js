529d6e828127f8c7350129c26f21daf5df4a1eaa
Chris Steinle <steinle.4400@gmail.com>
2018-03-21 14:17:22 -0700

    begin adding necessary components of game

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+console.log(commonWords)
+
+class Hangman {
+	constructor(){
+		this._answer = "word" //randomized, maybe ['w','o','r','d'] works better
+		this._guesses = [] //guessed letters, alphabetize when displayed
+	}
+}
